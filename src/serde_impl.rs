a1bf7e5290e6f61aacc6f01d26ce4915dd7bc0c0
Tom Parker <palfrey@tevp.net>
2016-10-08 15:45:13 +0100

    Upgrade to Serde 0.8

--- src/serde_impl.rs
+++ src/serde_impl.rs
@@ -1,50 +1,13 @@
 use serde::{ser, de};
 use super::{Timestamp, WallT};
-use time;
 
 impl<T: ser::Serialize> ser::Serialize for Timestamp<T> {
     fn serialize<S: ser::Serializer>(&self, serializer: &mut S) -> Result<(), S::Error> {
-        struct Visitor<'a, T: ser::Serialize + 'a> {
-            state: usize,
-            value: &'a Timestamp<T>,
-            _structure_ty: ::std::marker::PhantomData<&'a Timestamp<T>>,
-        }
-        impl<'a, T: ser::Serialize + 'a> ser::SeqVisitor for Visitor<'a, T> {
-            #[inline]
-            fn visit<S>(&mut self,
-                        serializer: &mut S)
-                        -> ::std::result::Result<Option<()>, S::Error>
-                where S: ser::Serializer
-            {
-                match self.state {
-                    0usize => {
-                        self.state += 1;
-                        Ok(Some(try!(serializer.serialize_tuple_struct_elt(&self.value.epoch))))
-                    }
-                    1usize => {
-                        self.state += 1;
-                        Ok(Some(try!(serializer.serialize_tuple_struct_elt(&self.value.time))))
-                    }
-                    2usize => {
-                        self.state += 1;
-                        Ok(Some(try!(serializer.serialize_tuple_struct_elt(&self.value.count))))
-                    }
-                    _ => Ok(None),
-                }
-            }
-            #[inline]
-            fn len(&self) -> Option<usize> {
-                Some(3usize)
-            }
-        }
-
-        serializer.serialize_tuple_struct("Timestamp",
-                                          Visitor {
-                                              value: self,
-                                              state: 0,
-                                              _structure_ty:
-                                                  ::std::marker::PhantomData::<&Timestamp<T>>,
-                                          })
+        let mut tuple_state = try!(serializer.serialize_tuple_struct("Timestamp", 3usize));
+        try!(serializer.serialize_tuple_struct_elt(&mut tuple_state, &self.epoch));
+        try!(serializer.serialize_tuple_struct_elt(&mut tuple_state, &self.time));
+        try!(serializer.serialize_tuple_struct_elt(&mut tuple_state, &self.count));
+        return serializer.serialize_tuple_struct_end(tuple_state);
     }
 }
 impl<T: de::Deserialize> de::Deserialize for Timestamp<T> {
@@ -98,38 +61,9 @@ impl<T: de::Deserialize> de::Deserialize for Timestamp<T> {
 
 impl ser::Serialize for WallT {
     fn serialize<S: ser::Serializer>(&self, serializer: &mut S) -> Result<(), S::Error> {
-        struct Visitor<'a> {
-            state: usize,
-            value: &'a WallT,
-            _structure_ty: ::std::marker::PhantomData<&'a WallT>,
-        }
-        impl<'a> ser::SeqVisitor for Visitor<'a> {
-            #[inline]
-            fn visit<S>(&mut self,
-                        serializer: &mut S)
-                        -> ::std::result::Result<Option<()>, S::Error>
-                where S: ser::Serializer
-            {
-                match self.state {
-                    0usize => {
-                        self.state += 1;
-                        Ok(Some(try!(serializer.serialize_tuple_struct_elt(&self.value.0))))
-                    }
-                    _ => Ok(None),
-                }
-            }
-            #[inline]
-            fn len(&self) -> Option<usize> {
-                Some(1usize)
-            }
-        }
-
-        serializer.serialize_tuple_struct("WallT",
-                                          Visitor {
-                                              value: self,
-                                              state: 0,
-                                              _structure_ty: ::std::marker::PhantomData::<&WallT>,
-                                          })
+        let mut tuple_state = try!(serializer.serialize_tuple_struct("WallT", 1usize));
+        try!(serializer.serialize_tuple_struct_elt(&mut tuple_state, &self.0));
+        return serializer.serialize_tuple_struct_end(tuple_state);
     }
 }
 impl de::Deserialize for WallT {
