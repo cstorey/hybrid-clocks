3a6104b5c6ce48b0de89068543e231988be3a61a
Ceri Storey <cez@necrofish.org.uk>
2019-05-05 17:04:09 +0100

    Refactor to use a Sink.

--- hlc-demo/src/main.rs
+++ hlc-demo/src/main.rs
@@ -10,17 +10,16 @@ extern crate env_logger;
 extern crate rand;
 extern crate serde_json;
 
-use std::fmt;
 use std::net::SocketAddr;
 use std::time::{Duration, Instant};
 
 use failure::Error;
-use futures::{Future, Stream};
-use hybrid_clocks::{Clock, Timestamp, Wall, WallT};
+use futures::prelude::*;
+use futures::{Future, Sink, Stream};
+use hybrid_clocks::{Clock, Timestamp, WallT};
 use rand::Rng;
 use structopt::StructOpt;
 use tokio::net::UdpSocket;
-use tokio::prelude::*;
 
 #[derive(Debug, StructOpt)]
 #[structopt(name = "hlc-demo", about = "Hybrid Logical clocks demo")]
@@ -35,11 +34,9 @@ struct Listener {
     socket: UdpSocket,
 }
 
-struct Client<S> {
-    notifications: S,
+struct Client {
     to_send: Option<(Timestamp<WallT>, SocketAddr)>,
     socket: UdpSocket,
-    clock: Clock<Wall>,
     peers: Vec<SocketAddr>,
 }
 
@@ -57,38 +54,31 @@ impl Future for Listener {
     }
 }
 
-impl<
-        S: Stream<Item = I, Error = E>,
-        I: fmt::Debug,
-        E: 'static + Send + Sync + std::error::Error,
-    > Future for Client<S>
-{
-    type Item = ();
-    type Error = Error;
+impl Sink for Client {
+    type SinkItem = Timestamp<WallT>;
+    type SinkError = Error;
 
-    fn poll(&mut self) -> Poll<(), Error> {
-        loop {
-            if let Some((msg, peer)) = self.to_send.as_ref() {
-                let bs = serde_json::to_vec(&msg)?;
-                let slen = try_ready!(self.socket.poll_send_to(&bs, &peer));
-                info!("Sent {} ({} bytes) to {}", msg, slen, peer);
-                self.to_send = None;
-            }
+    fn start_send(&mut self, item: Self::SinkItem) -> StartSend<Self::SinkItem, Self::SinkError> {
+        if self.to_send.is_some() {
+            return Ok(AsyncSink::NotReady(item));
+        }
 
-            match try_ready!(self.notifications.poll()) {
-                None => {
-                    info!("Stream ended");
-                    return Ok(Async::Ready(()));
-                }
-                Some(n) => {
-                    info!("Notification: {:?}", n);
-                    let idx = rand::thread_rng().gen_range(0, self.peers.len());
-                    let peer = self.peers[idx].clone();
-                    let t = self.clock.now();
-                    debug!("Queueing to:{}; msg:{}", peer, t);
-                    self.to_send = Some((t, peer))
-                }
-            }
+        let idx = rand::thread_rng().gen_range(0, self.peers.len());
+        let peer = self.peers[idx].clone();
+        debug!("Queueing to:{}; msg:{}", peer, item);
+        self.to_send = Some((item, peer));
+        Ok(AsyncSink::Ready)
+    }
+
+    fn poll_complete(&mut self) -> Poll<(), Self::SinkError> {
+        if let Some((msg, peer)) = self.to_send.as_ref() {
+            let bs = serde_json::to_vec(&msg)?;
+            let slen = try_ready!(self.socket.poll_send_to(&bs, &peer));
+            info!("Sent {} ({} bytes) to {}", msg, slen, peer);
+            self.to_send = None;
+            Ok(Async::Ready(()))
+        } else {
+            Ok(Async::NotReady)
         }
     }
 }
@@ -105,22 +95,28 @@ fn main() -> Result<(), Error> {
     let listener = Listener { socket };
 
     let socket = UdpSocket::bind(&"0.0.0.0:0".parse().expect("parse 0"))?;
-    let notifications = tokio::timer::Interval::new(Instant::now(), Duration::from_secs(1));
+    let notifications = {
+        let mut clock = Clock::wall();
+        tokio::timer::Interval::new(Instant::now(), Duration::from_secs(1))
+            .map(move |_| clock.now())
+            .map_err(Into::into)
+    };
+
     let to_send = None;
     info!("Client socket on: {}", socket.local_addr()?);
     let clock = Clock::wall();
     let client = Client {
-        notifications,
         socket,
         to_send,
-        clock,
         peers: opt.peers,
     };
 
     tokio::run(
-        client
-            .select(listener)
+        notifications
+            .forward(client)
             .map(|_| ())
+            .select(listener)
+            .map(|(a, _)| a)
             .map_err(|(e, _)| e)
             .map_err(|e| println!("Listener error = {:?}", e)),
     );
